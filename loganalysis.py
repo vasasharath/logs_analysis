6ed9512b246de90227275d06ee202bf79ab76df2
sharath <sharathvasa@gmail.com>
2018-01-26 14:40:03 -0500

    Changing file to accept pep8 format

--- loganalysis.py
+++ loganalysis.py
@@ -6,23 +6,23 @@ DBNAME = "news"
 
 
 def get_popular_articles():
-    #Connecting to news database
+    # Connecting to news database
     db = psycopg2.connect(database=DBNAME)
     c = db.cursor()
-	#executing the query on database
+    # executing the query on database
     c.execute("""
 	           SELECT articles.title,COUNT(*) as num
-			   FROM articles,log
-			   WHERE log.path like concat('%article%',articles.slug)
-  			   GROUP BY 1
-			   ORDER BY 2 DESC
-			   LIMIT 3;
-			   """)
+               FROM articles,log
+               WHERE log.path like concat('%article%',articles.slug)
+               GROUP BY 1
+               ORDER BY 2 DESC
+               LIMIT 3;
+               """)
     rows = c.fetchall()
-	#closing database connection
+    # closing database connection
     db.close()
     print('\nThe 3 most popular articles of all time are\n')
-	#loop through the result and display each row
+    # loop through the result and display each row
     for article in rows:
         print(article[0] + ' — ' + str(article[1]) + ' views')
 
@@ -31,14 +31,14 @@ def get_popular_authors():
     db = psycopg2.connect(database=DBNAME)
     c = db.cursor()
     c.execute("""
-				SELECT authors.name, COUNT(*) AS num
-				FROM authors,articles,log
-				WHERE authors.id = articles.author
-				AND log.path like concat('/article/%', articles.slug)
-				GROUP BY 1
-				ORDER BY 2 DESC
-				LIMIT 3;
-			  """)
+                SELECT authors.name, COUNT(*) AS num
+                FROM authors,articles,log
+                WHERE authors.id = articles.author
+                AND log.path like concat('/article/%', articles.slug)
+                GROUP BY 1
+                ORDER BY 2 DESC
+                LIMIT 3;
+              """)
     rows = c.fetchall()
     db.close()
     print('\n\nThe most popular article authors of all time are\n')
@@ -50,10 +50,10 @@ def get_error_day():
     db = psycopg2.connect(database=DBNAME)
     c = db.cursor()
     c.execute("""
-	           SELECT *
-	           FROM error_log
-			   WHERE Error_percentage > 1;
-			  """)
+               SELECT *
+               FROM error_log
+               WHERE Error_percentage > 1;
+              """)
     rows = c.fetchall()
     db.close()
     print('\n\nDays with more than 1% of request that lead to an error\n')
