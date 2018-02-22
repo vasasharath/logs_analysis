1f08b59384707bf2af62756cf974d259c43cd680
sharath <sharathvasa@gmail.com>
2018-02-22 13:26:14 -0500

    docs: adding comments to improve code readability

--- loganalysis.py
+++ loganalysis.py
@@ -42,6 +42,7 @@ def get_popular_authors():
                 LIMIT 3;
               """)
     rows = c.fetchall()
+    # closing database connection
     db.close()
     print('\n\nThe most popular article authors of all time are\n')
     for author in rows:
